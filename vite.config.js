133197d707ebaa22422b68c29785bdc2142408ea
agustin <agustinpatat5@gmail.com>
2025-05-18 13:48:40 -0300

    Actualizacion

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,15 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: [
-        'react-toastify',
-        'react-icons',
-        'chart.js',
-        'react-chartjs-2'
-      ]
+  resolve: {
+    alias: {
+      'react-toastify': 'react-toastify',
+      'react-icons': 'react-icons',
+      'chart.js': 'chart.js',
+      'react-chartjs-2': 'react-chartjs-2'
     }
+  },
+  optimizeDeps: {
+    include: ['react-toastify', 'react-icons', 'chart.js', 'react-chartjs-2']
   }
 });
